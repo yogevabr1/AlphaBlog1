a2403ca3cb54c7ccaa63a01cfb8a26f86543d47e
Rails course <yogevabr@gmail.com>
2018-04-28 22:01:43 +0300

    initial

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'welcome/home', to: 'welcome#home'
+  get 'welcome/about', to: 'welcome#about'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
